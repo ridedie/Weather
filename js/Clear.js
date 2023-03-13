43ce942b5b7d05a99992fdf77d8a450c7d757b19
ridedie <ride_die@naver.com>
2023-03-13 23:58:51 +0900

    Upload

---
+++ js/Clear.js
@@ -0,0 +1,69 @@
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+
+function resize() {
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+}
+resize();
+
+function fStyle() {
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+
+document.addEventListener("mousemove", (e) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    ctx.beginPath();
+    ctx.arc(mouseX, mouseY, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.fillStyle = "red";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(mouseX, mouseY-150);
+    ctx.lineTo(mouseX, mouseY-70);
+    fStyle();
+
+    ctx.beginPath();
+    ctx.moveTo(mouseX+100, mouseY-100);
+    ctx.lineTo(mouseX+50, mouseY-50);
+    fStyle();
+
+    ctx.beginPath();
+    ctx.moveTo(mouseX+150, mouseY);
+    ctx.lineTo(mouseX+70, mouseY);
+    fStyle();
+
+    ctx.beginPath();
+    ctx.moveTo(mouseX+100, mouseY+100);
+    ctx.lineTo(mouseX+50, mouseY+50);
+    fStyle();
+
+    ctx.beginPath();
+    ctx.moveTo(mouseX, mouseY+150);
+    ctx.lineTo(mouseX, mouseY+70);
+    fStyle();
+
+    ctx.beginPath();
+    ctx.moveTo(mouseX-100, mouseY+100);
+    ctx.lineTo(mouseX-50, mouseY+50);
+    fStyle();
+
+    ctx.beginPath();
+    ctx.moveTo(mouseX-150, mouseY);
+    ctx.lineTo(mouseX-70, mouseY);
+    fStyle();
+
+    ctx.beginPath();
+    ctx.moveTo(mouseX-100, mouseY-100);
+    ctx.lineTo(mouseX-50, mouseY-50);
+    fStyle();
+})
+
